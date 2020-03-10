fc3e70b679748bffbf1e84fde1e52880ba16eeed
pierremrcr <pierremercier4@gmail.com>
2020-03-10 20:18:34 +0100

    Classe EscargotDAO updated

--- cargot/cargot-ejb/src/main/java/aliptic/projet/cargot/internal/daos/EscargotDAO.java
+++ cargot/cargot-ejb/src/main/java/aliptic/projet/cargot/internal/daos/EscargotDAO.java
@@ -1,37 +1,47 @@
 package aliptic.projet.cargot.internal.daos;
 
 import java.util.List;
+
 import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+
+import aliptic.projet.cargot.internal.Calibre;
+import aliptic.projet.cargot.internal.Espece;
 import aliptic.projet.cargot.internal.entities.EscargotEntity;
 
 @Stateless
 @Local
 public class EscargotDAO {
 	
-	@PersistenceContext(unitName="A_MODIFIER")
+	@PersistenceContext(unitName="cargotPersistenceUnit")
 	EntityManager entityManager;
 
-	public void create(EscargotEntity Escargot) {
-		entityManager.persist(Escargot);
+	public void createEscargot(int id, boolean isDisponible, Calibre calibre, Espece espece, double poids) {
+		EscargotEntity escargot = new EscargotEntity(id, isDisponible, calibre, espece, poids);
+		entityManager.persist(escargot);
 	}
 
-	public EscargotEntity readOne(int id) {
-		return entityManager.find(EscargotEntity.class, id);
+	public EscargotEntity getEscargotById(int id) {
+		return (EscargotEntity) entityManager.createQuery("Select p from EscargotEntity p where p.id=:id").setParameter("id",id).getSingleResult();
 	}
 	
-	public List<EscargotEntity> readAll() {
-		return entityManager.createQuery("from EscargotEntity").getResultList();
+	public List<EscargotEntity> getAllEscargot() {
+		TypedQuery<EscargotEntity> query = entityManager.createQuery("Select p from EscargotEntity p",EscargotEntity.class);
+		List<EscargotEntity> escargots = query.getResultList();
+		return escargots;
 	}
 
-	public void update(EscargotEntity Escargot) {
-		entityManager.persist(Escargot);
+	public void modifierEscargot(EscargotEntity Escargot) {
+		entityManager.merge(Escargot);
 	}
 
-	public void delete(EscargotEntity Escargot) {
-		entityManager.remove(Escargot);
+	public void deleteEscargot(int id) {
+		EscargotEntity escargot = getEscargotById(id);
+		entityManager.remove(escargot);
 	}
 
 }
