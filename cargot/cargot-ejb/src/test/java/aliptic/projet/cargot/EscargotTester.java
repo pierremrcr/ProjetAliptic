4682c5e3a6d1725cee5e614ef00b38de61b7eba9
pierremrcr <pierremercier4@gmail.com>
2020-03-11 15:28:27 +0100

    Test Class Updated

--- cargot/cargot-ejb/src/test/java/aliptic/projet/cargot/EscargotTester.java
+++ cargot/cargot-ejb/src/test/java/aliptic/projet/cargot/EscargotTester.java
@@ -9,15 +9,14 @@ import javax.naming.NamingException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import aliptic.projet.cargot.pub.services.EscargotServiceRemote;
-import aliptic.projet.cargot.pub.dtos.EscargotDTO;
 import aliptic.projet.cargot.internal.Calibre;
 import aliptic.projet.cargot.internal.Espece;
 import aliptic.projet.cargot.internal.entities.EscargotEntity;
 
 public class EscargotTester {
-	
+
 	protected static InitialContext context;
-	
+
 	@BeforeClass
 	public static void testInit() throws NamingException {
 		Hashtable<Object, Object> props = new Hashtable<>();
@@ -32,13 +31,13 @@ public class EscargotTester {
 		props.put(Context.PROVIDER_URL, "http-remoting://127.0.0.1:8080");
 		props.put("remote.connection.default.host", "127.0.0.1");
 		props.put("remote.connection.default.port", "8080");
-//		props.put(Context.PROVIDER_URL,"remote://localhost:4447");
-//		props.put(Context.SECURITY_PRINCIPAL, "testuser");
-//		props.put(Context.SECURITY_CREDENTIALS, "testpassword");
+		//		props.put(Context.PROVIDER_URL,"remote://localhost:4447");
+		//		props.put(Context.SECURITY_PRINCIPAL, "testuser");
+		//		props.put(Context.SECURITY_CREDENTIALS, "testpassword");
 		props.put("jboss.naming.client.ejb.context", true);
 		context = new InitialContext(props);
 	}
-	
+
 	@Test
 	public void testEscargotServiceRemote() throws NamingException {
 		testInit();
@@ -46,10 +45,14 @@ public class EscargotTester {
 		service.createEscargot(1, true, Calibre.PETIT, Espece.BOURGOGNE, 5.);
 		EscargotEntity Escargot = service.getEscargotById(1);
 		Escargot.setPoids(21.5);
-		service.updateEscargot(Escargot);
+		service.updateEscargot(1, true, Calibre.PETIT, Espece.PETIT_GRIS, 23.5);
 		service.createEscargot(2, true, Calibre.MOYEN, Espece.PETIT_GRIS, 2.5);
 		service.createEscargot(3, true, Calibre.GRAND, Espece.BOURGOGNE, 25.);
-		List<EscargotEntity> Escargots = service.getAllEscargots();
+		List<EscargotEntity> escargots = service.getAllEscargots();
+		for (EscargotEntity escargot : escargots){
+			System.out.println(escargot.getId() + " " + escargot.getCalibre() + " " + escargot.getEspece());
+
+		}
 	}
 
 
