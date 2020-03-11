3d95c0b9ac294fa451da6093dd79c990f7d1fa43
pierremrcr <pierremercier4@gmail.com>
2020-03-11 15:16:07 +0100

    création méthodes VerrineService

--- cargot/cargot-ejb/src/main/java/aliptic/projet/cargot/internal/daos/VerrineDAO.java
+++ cargot/cargot-ejb/src/main/java/aliptic/projet/cargot/internal/daos/VerrineDAO.java
@@ -5,6 +5,11 @@ import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import aliptic.projet.cargot.internal.Calibre;
+import aliptic.projet.cargot.internal.Espece;
+import aliptic.projet.cargot.internal.entities.EscargotEntity;
 import aliptic.projet.cargot.internal.entities.VerrineEntity;
 
 @Stateless
@@ -14,24 +19,32 @@ public class VerrineDAO {
 	@PersistenceContext(unitName="cargotPersistenceUnit")
 	EntityManager entityManager;
 
-	public void create(VerrineEntity Verrine) {
-		entityManager.persist(Verrine);
+	public void create(int id, Calibre calibre, Espece espece, int quantiteMax, List<EscargotEntity> escargots) {
+		VerrineEntity verrine = new VerrineEntity(id, calibre, espece, quantiteMax, escargots);
+		entityManager.persist(verrine);
 	}
 
-	public VerrineEntity readOne(int id) {
-		return entityManager.find(VerrineEntity.class, id);
+	public VerrineEntity getVerrineById(int id) {
+		return (VerrineEntity) entityManager.createQuery("Select v from VerrineEntity v where v.id=:id").setParameter("id",id).getSingleResult();
 	}
 	
-	public List<VerrineEntity> readAll() {
-		return entityManager.createQuery("from VerrineEntity").getResultList();
+	public List<VerrineEntity> getAllVerrines() {
+		TypedQuery<VerrineEntity> query = entityManager.createQuery("Select p from VerrineEntity p",VerrineEntity.class);
+		List<VerrineEntity> verrines = query.getResultList();
+		return verrines;
 	}
 
-	public void update(VerrineEntity Verrine) {
-		entityManager.persist(Verrine);
+	public void update(VerrineEntity verrine) {
+		entityManager.merge(verrine);
 	}
 
 	public void delete(VerrineEntity Verrine) {
 		entityManager.remove(Verrine);
+	}
+	
+	public void deleteVerrineById(int id) {
+		VerrineEntity verrine = getVerrineById(id);
+		entityManager.remove(verrine);
 	}
 
 }
--- cargot/cargot-ejb/src/main/java/aliptic/projet/cargot/pub/services/VerrineServiceRemote.java
+++ cargot/cargot-ejb/src/main/java/aliptic/projet/cargot/pub/services/VerrineServiceRemote.java
@@ -2,16 +2,21 @@ package aliptic.projet.cargot.pub.services;
 
 import java.util.List;
 import javax.ejb.Remote;
+
+import aliptic.projet.cargot.internal.Calibre;
+import aliptic.projet.cargot.internal.Espece;
+import aliptic.projet.cargot.internal.entities.EscargotEntity;
+import aliptic.projet.cargot.internal.entities.VerrineEntity;
 import aliptic.projet.cargot.pub.dtos.VerrineDTO;
 
 @Remote
 public interface VerrineServiceRemote {
 	
-	public void createVerrine(VerrineDTO Verrine);
-	public VerrineDTO readVerrine(int id);
-	public List<VerrineDTO> readVerrines();
-	public void updateVerrine(VerrineDTO Verrine);
-	public void deleteVerrine(VerrineDTO Verrine);
-	public void deleteVerrineFromId(int id);
+	public void createVerrine(int id, Calibre calibre, Espece espece, int quantiteMax, List<EscargotEntity> escargots);
+	public VerrineDTO getVerrineById(int id);
+	public List<VerrineDTO> getAllVerrines();
+	public void updateVerrine(int id, Calibre calibre, Espece espece, int quantiteMax, List<EscargotEntity> escargots);
+	public void deleteVerrine(VerrineEntity Verrine);
+	public void deleteVerrineById(int id);
 
 }
